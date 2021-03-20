bf7126f5ad7b5a489c80442adba73771395ca1dd
massive2351 <massive2351@gmail.com>
2021-03-20 09:10:05 +0000

    deviseでユーザ登録・ログイン機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
   private
   
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, images: [])
   end
   
   def find_post
